35a136aecfd8325f867fc993be723769d14b5ab4
Andrew Turner <ajturner360@gmail.com>
2022-02-04 00:40:51 -0500

    Lab 3 solution

--- assignments/hw3/hw3.py
+++ assignments/hw3/hw3.py
@@ -1,34 +1,57 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
-
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
+Name: Andrew Turner
+hw3.py
 
 Certification of Authenticity:
-<include one of the following>
 I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
 """
 
+import math
 
 def average():
-    pass
+    accumulator = 0
+    numberof = eval(input("How many values do you plan on entering?"))
+    for value in range(numberof):
+        numba = eval(input("Input value:"))
+        accumulator = numba + accumulator
+    print(accumulator/numberof)
 
 
 def tip_jar():
-    pass
+    tipaccumulator = 0
+    for person in range(5):
+        tip = eval(input("how much would you like to donate?"))
+        tipaccumulator = tipaccumulator + tip
+    print("total tips:", tipaccumulator)
 
 
 def newton():
-    pass
+    number = eval(input("what number would you like to square root?"))
+    times = eval(input("how many times would you like to improve the approximation?"))
+    approx = number
+    for i in range(times):
+        approx = (approx + number/approx)/2
+    print("the square root is approximately", approx)
+
 
 
 def sequence():
-    pass
+    terms = eval(input("How many terms would you like?"))
+    for i in range(1, terms+1):
+        print(i - abs(i%2 - 1), end=" ")
+
+
+
 
 
 def pi():
-    pass
+    terms = eval(input("How many terms in the series?"))
+    piaccum = 1
+    for i in range(1, terms+1):
+        numaccum = (i+abs(i%2))
+        denomaccum = (i+abs(i%2-1))
+        piaccum = numaccum/denomaccum * piaccum
+    print(piaccum*2)
 
 
 if __name__ == '__main__':
