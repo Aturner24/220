46717009c3508cc41b84c9c1bca2b28b85780a79
Andrew Turner <ajturner360@gmail.com>
2022-03-04 23:01:10 -0500

    Lab 7 solution

--- assignments/hw7/hw7.py
+++ assignments/hw7/hw7.py
@@ -12,23 +12,45 @@ I certify that this assignment is my own work, but I discussed it with: <Name(s)
 
 
 def number_words(in_file_name, out_file_name):
-    pass
+    infile = open(in_file_name, "r")
+    outfile = open(out_file_name, "w")
+    linenumber = 0
+    for line in infile:
+        splitlines = line.split(' ')
+        for word in splitlines:
+            linenumber = linenumber+1
+            outfile.write(str(linenumber)+" " + word+"\n")
+    infile.close()
+    outfile.close()
+
 
 
 def hourly_wages(in_file_name, out_file_name):
-    pass
-
+    infile = open(in_file_name, 'r')
+    outfile = open(out_file_name, 'w')
+    for line in infile:
+        listworker = line.split(' ')
+        weeklypayment = (float(listworker[2])+1.65) * float(listworker[3])
+        weeklypayment = '${:.2f}'.format(weeklypayment)
+        outfile.write(str(listworker[0])+' '+str(listworker[1]) + " " + weeklypayment + '\n')
+    infile.close()
+    outfile.close()
 
 def calc_check_sum(isbn):
     pass
-
+    "ask about these in class "
 
 def send_message(file_name, friend_name):
     pass
 
-
 def encode():
-    pass
+    message = input("Enter a message to be encoded")
+    key = eval(input("Enter a key to encode the message with"))
+    codedmessage = ''
+    for letter in message:
+        c = ord(letter) + key
+        codedmessage = codedmessage + chr(c)
+    print(codedmessage)
 
 
 def send_safe_message(file_name, friend_name, key):
