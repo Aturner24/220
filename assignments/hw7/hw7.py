838d7e7f18ba3108b2993c3ecf925692292f6842
Andrew Turner <ajturner360@gmail.com>
2022-03-04 23:02:20 -0500

    Homework 7 solution

--- assignments/hw7/hw7.py
+++ assignments/hw7/hw7.py
@@ -1,13 +1,8 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: Andrew Turner
+<hw7>.py
 
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
-
-Certification of Authenticity:
-<include one of the following>
 I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
 """
 
 
